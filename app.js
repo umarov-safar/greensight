84eaa5235d4adcaa64a788161b5be3d9573e2483
Safar Umarov <safarumarov711@gmail.com>
2024-05-04 04:48:24 +0700

    js validation and request

---
+++ app.js
@@ -0,0 +1,79 @@
+const form = document.getElementById("register-form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let formData = new FormData(form);
+    formData = Object.fromEntries(formData);
+    clearNotifiedErrors(formData);
+
+    if (validateRequest(formData)) {
+        sendRequest(formData);
+    }
+});
+
+const sendRequest = (formData) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'server.php');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = () => {
+        if (xhr.status === 200 || xhr.status === 201) {
+            console.log(xhr.response);
+            console.log(xhr);
+        }
+    }
+    xhr.onerror = function() {
+        alert(`Ошибка соединения`);
+    };
+    xhr.send(JSON.stringify(formData));
+}
+
+
+// Basic validation.
+const validateRequest = (data) => {
+    // this contains all feiled fields
+    const errors = {};
+
+    if (data.name.length === 0) {
+        errors.name = "Это поле обязательно для заполнения!";
+    }
+    if (data.last_name.length === 2) {
+        errors.last_name = "Это поле обязательно для заполнения!"
+    }
+    // Here is a simple validation for email. The advance validation will be in server
+    if (! data.email.includes('@')) {
+        errors.email = "Неправильный формат!"
+    }
+    if (data.password.length >= 4) {
+        if (data.password !== data.confirmation_password) {
+            errors.confirmation_password = "Пароль не совпадает!"
+        }
+    } else {
+        errors.password = "Пароль должно быть больше 4 символ!"
+    }
+
+    if (!isObjectEmpty(errors)) {
+        notifyValidationErrors(errors);
+        return false;
+    }
+
+    return true;
+}
+
+const notifyValidationErrors = (errors) => {
+    const iterableErrors = Object.entries(errors);
+    iterableErrors.forEach(([name, message]) => {
+        document.querySelector(`#error_${name}`).innerText = message;
+    })
+}
+
+const clearNotifiedErrors = (formData) => {
+    const fields = Object.keys(formData);
+    fields.forEach((field) => {
+        document.querySelector(`#error_${field  }`).innerText = '';
+    })
+}
+
+const isObjectEmpty = (objectName) => {
+    return Object.keys(objectName).length === 0
+}
